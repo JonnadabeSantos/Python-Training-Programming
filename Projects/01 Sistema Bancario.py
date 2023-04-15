26c149efe9c7e881c4300c6d520f8c8239ccd1ba
JonnadabeSantos <dabedeveloper@gmail.com>
2023-04-15 02:15:45 -0300

    Saque Validado

--- Projects/01 Sistema Bancario.py
+++ Projects/01 Sistema Bancario.py
@@ -2,11 +2,25 @@ cheque_especial = 600
 saldo_conta_corrente = 2000
 saldo_conta_universitaria = 950
 
-menu = """
+
+
+menu_geral = """
 [01] Conta Corrente 
 [02] Conta Universitária
 
-Selecione o Tipo de conta: """
+Selecione o Tipo de conta:"""
+
+
+
+menu_conta_corrente = """
+[01] Saque:
+[02] Depósito:
+[03] Saldo:
+[04] Saldo Cheque Especial:
+
+Selecione a Operação: """
+
+
 
 
 operacao_principal = "inicie"
@@ -16,13 +30,16 @@ while operacao_principal == "inicie":
     operacao_principal = "pare"
     reset_programa = "pare"
 
+    reset_operacao_conta_corrente = "pare"
+
+
     conta_corrente = False
     conta_universitaria = False      
 
 
 # Validação Funcionou ==============================
    
-    selecione_conta = input(f'{menu} ')
+    selecione_conta = input(f'{menu_geral} ')
     if selecione_conta.isdigit():
         selecione_conta = int(selecione_conta)
         
@@ -46,115 +63,135 @@ while operacao_principal == "inicie":
         
         reset_operacao_conta_corrente = "inicie"
         while reset_operacao_conta_corrente == "inicie":
-        
-            operacao_conta_corrente = int(input(
-"""
-[01] Saque:
-[02] Depósito:
-[03] Saldo:
-[04] Saldo Cheque Especial:
+            
+# Validação Funcionou =========================================================================================================
 
-Selecione a Operação: """))       
+            operacao_conta_corrente = input(f'{menu_conta_corrente} ')
+            if operacao_conta_corrente.isdigit():
+                operacao_conta_corrente = int(operacao_conta_corrente)
 
-           
-            if operacao_conta_corrente >= 1 and operacao_conta_corrente <= 4:
-                reset_operacao_conta_corrente = "pare"
-                  
-            if operacao_conta_corrente == 1:
-                print("\nSaque Selecionado!")
+#=============================================================================================================================
+
                 
-                saque_conta_corrente = int(input("Informe o Valor do Saque em sua conta corrente: "))
+                if operacao_conta_corrente >= 1 and operacao_conta_corrente <= 4:
+                    reset_operacao_conta_corrente = "pare"
+                  
+                if operacao_conta_corrente == 1:
+                    print("\nSaque Selecionado!")
+                                        
+#Validação Funcionou =========================================================================================================
                     
-                if saque_conta_corrente > (saldo_conta_corrente + cheque_especial):
-                    print("\nSaldo Insuficiênte !")
-                    reset_programa = "inicie"
-
-
-
-
-                elif saque_conta_corrente > saldo_conta_corrente and saque_conta_corrente <= (saldo_conta_corrente + cheque_especial):
-                    print("\nVocê está usando",saque_conta_corrente - saldo_conta_corrente,"do seu Cheque especial deseja continuar? ")
-                    
-                    reset_sim_ou_nao = "inicie"
-                    while reset_sim_ou_nao == "inicie":
-                        continuar_saque = input("Degite sim ou não: ")
-                        continuar_saque = continuar_saque.lower()
-                   
-
-                        if continuar_saque == "sim":
-                            
-                            cheque_especial -= (saque_conta_corrente - saldo_conta_corrente) 
-                            saldo_conta_corrente = 0
-                            
-                            print("\nSaque realizado! seu saldo é",saldo_conta_corrente)                                   
-                            print("O Valor do seu Cheque Especial é",cheque_especial)
-                            reset_sim_ou_nao = "pare"
-                            reset_programa = "inicie"
-                    
-                            
-                        elif continuar_saque == "nao" or continuar_saque == "não":
-                            print("\nSaque Não Realizado !")
-                            reset_sim_ou_nao = "pare"
-                            reset_programa = "inicie"
+                    reset_saque_conta_corrente = 'inicie'
+                    while reset_saque_conta_corrente == 'inicie':
                         
+                        saque_conta_corrente = input("Informe o Valor do Saque em sua conta corrente: ")
+                        if saque_conta_corrente.isdigit():
+                            saque_conta_corrente = int(saque_conta_corrente)
+                            
+#=============================================================================================================================
 
+
+                            if saque_conta_corrente > (saldo_conta_corrente + cheque_especial):
+                                print("\nSaldo Insuficiênte !")
+                                reset_saque_conta_corrente = 'pare'
+                                reset_programa = "inicie"
+                               
+
+
+                            elif saque_conta_corrente > saldo_conta_corrente and saque_conta_corrente <= (saldo_conta_corrente + cheque_especial):
+                                print("\nVocê está usando",(saque_conta_corrente - saldo_conta_corrente),"do seu Cheque especial deseja continuar? ")
+                                reset_saque_conta_corrente = 'pare'
+                                
+                                reset_sim_ou_nao = "inicie"
+                                while reset_sim_ou_nao == "inicie":
+                                    continuar_saque = input("Escreva sim ou não: ").lower()
+                                                              
+
+                                    if continuar_saque == "sim":
+                                        
+                                        cheque_especial -= (saque_conta_corrente - saldo_conta_corrente) 
+                                        saldo_conta_corrente = 0
+                                        
+                                        print("\nSaque realizado! seu saldo é",saldo_conta_corrente)                                   
+                                        print("O Valor do seu Cheque Especial é",cheque_especial)
+                                        reset_sim_ou_nao = "pare"
+                                        reset_programa = "inicie"
+                                       
+                                        
+                                    elif continuar_saque == "nao" or continuar_saque == "não":
+                                        print("\nSaque Não Realizado !")
+                                        reset_sim_ou_nao = "pare"
+                                        reset_programa = "inicie"
+                                        
+
+
+                                    else:
+                                        print("\nOpção Inválida! ")
+                                                       
+                                        
+
+
+                            
+
+                            elif saque_conta_corrente <= saldo_conta_corrente and saque_conta_corrente > 0:
+                                saldo_conta_corrente -= saque_conta_corrente
+                                print("\nSaque de",saque_conta_corrente,"realizado! seu saldo é",saldo_conta_corrente)
+                                reset_saque_conta_corrente = 'pare'
+                                reset_programa = "inicie"
+
+
+                        
+                            else:
+                                print("\nValor Inválido! ")
+                        
+                        
+                        
                         else:
-                            print("\nOpção Inválida! ")
-                            reset_sim_ou_nao = "inicie"                 
-
-
-                    
-
-                elif saque_conta_corrente <= saldo_conta_corrente and saque_conta_corrente > 0:
-                    saldo_conta_corrente -= saque_conta_corrente
-                    print("\nSaque de",saque_conta_corrente,"realizado! seu saldo é",saldo_conta_corrente)
-                    reset_programa = "inicie"
-                    
-        
+                            print("\nOpção Inválida! Digite apenas números !")
             
 
     # Deposito conta corrente
             
 
-            elif operacao_conta_corrente == 2:               
-                
-                print("Depósito Selecionado! \n")
-                deposito_conta_corrente = int(input("Informe o valor do depósito: "))
-
-                reset_deposito_conta_corrente = "inicie"
-                while reset_deposito_conta_corrente == "inicie":
+                elif operacao_conta_corrente == 2:               
                     
+                    print("Depósito Selecionado! \n")
+                    deposito_conta_corrente = int(input("Informe o valor do depósito: "))
 
-                    if deposito_conta_corrente > 0:
-                        saldo_conta_corrente += deposito_conta_corrente
-                        print("\nDepósito de",deposito_conta_corrente,"Realizado! seu saldo é",saldo_conta_corrente)                                                                
-                        reset_deposito_conta_corrente = "pare"
-                        reset_programa = "inicie"
-                
-                    else:
-                        print("\nOpção Inválida!")
-                        deposito_conta_corrente = int(input("Informe Novamente o valor do depósito: "))
+                    reset_deposito_conta_corrente = "inicie"
+                    while reset_deposito_conta_corrente == "inicie":
+                        
+
+                        if deposito_conta_corrente > 0:
+                            saldo_conta_corrente += deposito_conta_corrente
+                            print("\nDepósito de",deposito_conta_corrente,"Realizado! seu saldo é",saldo_conta_corrente)                                                                
+                            reset_deposito_conta_corrente = "pare"
+                            reset_programa = "inicie"
+                    
+                        else:
+                            print("\nOpção Inválida!")
+                            deposito_conta_corrente = int(input("Informe Novamente o valor do depósito: "))
 
                        
 
     # Saldo conta corrente
 
-            elif operacao_conta_corrente == 3:
-                print("\nSaldo Selecionado! \n")
-                print("Seu saldo é",saldo_conta_corrente,"\n")
-                reset_programa = "inicie"
+                elif operacao_conta_corrente == 3:
+                    print("\nSaldo Selecionado! \n")
+                    print("Seu saldo é",saldo_conta_corrente,"\n")
+                    reset_programa = "inicie"
 
 
     # Saldo Cheque especial
 
-            elif operacao_conta_corrente == 4:
-                print("\nSaldo de Cheque Especial Selecionado! \n")
-                print("Seu saldo em cheque especial é",cheque_especial,"\n")
-                reset_programa = "inicie"
-                
-                
-            else:
-                print("\nOperação Inválida!") 
+                elif operacao_conta_corrente == 4:
+                    print("\nSaldo de Cheque Especial Selecionado! \n")
+                    print("Seu saldo em cheque especial é",cheque_especial,"\n")
+                    reset_programa = "inicie"
+                    
+                    
+                else:
+                    print("\nOperação Inválida!") 
             
 
 
