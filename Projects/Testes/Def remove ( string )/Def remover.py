e6b8867893ae3cb0ae8df8b1e21f5db66197aece
JonnadabeSantos <dabedeveloper@gmail.com>
2023-04-16 00:17:28 -0300

    Criete Def remover

---
+++ Projects/Testes/Def remove ( string )/Def remover.py
@@ -0,0 +1,18 @@
+
+# Geral
+def remove(string): 
+    return "".join(string.split()) 
+      
+string = ' g e e k '
+print(remove(string)) 
+
+
+# Aplicação
+
+n2 = 'metimozerque napomuceno cardoso cavalcante'
+
+def remove(n2): 
+    return "-".join(n2.split()) 
+      
+n2 = n2
+print(remove(n2)) 
