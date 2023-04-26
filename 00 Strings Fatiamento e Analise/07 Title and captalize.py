9eb9efde2cedc6fda4f15eca7b1c74964c9e8c58
JonnadabeSantos <dabedeveloper@gmail.com>
2023-04-26 10:36:55 -0300

    Fat

---
+++ 00 Strings Fatiamento e Analise/07 Title and captalize.py
@@ -0,0 +1,6 @@
+n1 = 'valdomiro silva cacete'
+n2 = 'metimozerque napomuceno cardoso cavalcante'
+n3 = 'josadarque pereira de pinto sega'
+
+print(n2.capitalize()) #leave only the first
+print(n3.title())
