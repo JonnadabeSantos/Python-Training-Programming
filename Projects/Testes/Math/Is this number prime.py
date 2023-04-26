36f152e365bcc9cd6e8583b390efe70d3c533b74
JonnadabeSantos <dabedeveloper@gmail.com>
2023-04-26 11:20:38 -0300

    Full test

--- Projects/Testes/Math/Is this number prime.py
+++ Projects/Testes/Math/Is this number prime.py
@@ -1,5 +1,15 @@
-a = 47 # Is this number primer ?
+a = 85 # Is this number primer ?
+prime = 0
 
 for soma in range(1, a+1):
     rest = a % soma
     print(soma, rest)
+    
+    if rest == 0:
+        prime += 1
+
+if prime == 2:
+    print(f'Numero {a} é primo')
+
+else:
+    print(f'Numero {a} não é primo')
