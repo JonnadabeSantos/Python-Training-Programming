2368e9b00ca4a57d5e56696f8d85e6ea8b78eb5d
JonnadabeSantos <dabedeveloper@gmail.com>
2023-05-14 19:18:02 -0300

    Create Avegander

--- Exercise/Guanabara/Two_World/User - randit 999.py
+++ Exercise/Guanabara/Two_World/User - randit 999.py
@@ -1,13 +1,10 @@
-from random import randint
-
 soma = 0
 cont = 0
 user = int(input('Type Number: '))
 
 while not user == 999:
-    Ai = randint(0,999)
-    print(f'\n{Ai}')
-    soma += Ai
+    print(f'\n{user}')
+    soma += user
     cont += 1
     user = int(input('Type Number: '))
     
