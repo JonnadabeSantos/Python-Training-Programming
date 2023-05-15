ef309e257e0672712bf312a5dac7f8446f1a9c6e
JonnadabeSantos <dabedeveloper@gmail.com>
2023-05-15 10:42:05 -0300

    New File

---
+++ man = woman = people = 0.py
@@ -0,0 +1,43 @@
+man = woman = people = 0
+
+cadastro = ' '
+while cadastro in 'Nn':
+    print('='*60)
+    print()
+
+    menu = 'CADASTRO DE PESSOAS'
+    print(menu.center(60, ' '))
+    print() 
+
+    idade = int(input('Type Years Old: '))
+    sexo = input('Inform your gender [M/F]: ').strip() [0]
+
+    if idade > 18:
+        people += 1
+    
+    if sexo in 'Mm':
+        man += 1
+
+    if sexo in 'Ff' and idade < 20:
+        woman += 1
+    
+    print()
+    cadastro = input('Do you wish to continue ? [S/N]: ').strip() [0]
+
+
+print('='*60)
+print()
+
+menu = 'TOTAL DE PESSOAS CADASTRADAS'
+print(menu.center(60, ' '))
+print() 
+
+print(f'''
+Maiores de 18 Anos:             {people}
+Homens:                         {man}
+Mulheres menores de 20 Anos:    {woman}
+''') 
+
+  
+
+
