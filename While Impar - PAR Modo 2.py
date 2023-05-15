15435e41b7f1e99324eb9c1b01c8483abac48815
JonnadabeSantos <dabedeveloper@gmail.com>
2023-05-14 23:05:52 -0300

    Create While IMpar par

--- While Impar - PAR Modo 2.py
+++ While Impar - PAR Modo 2.py
@@ -14,7 +14,7 @@ while True:
         if soma % 2 == 0:
             print(f'Você é par escolheu o Numero {number} e seu oponente {Ai} logo {soma} é Par Voce Ganhou !\n')
             cont += 1
-            # soma = 0
+            soma = 0
         else:
             print(f'Você é par escolheu o Numero {number} e seu oponente {Ai} logo {soma} é Impar Voce perdeu !\n')
             break
@@ -23,7 +23,7 @@ while True:
         if soma % 2 == 1:
             print(f'Você é impar escolheu o Numero {number} e seu oponente {Ai} logo {soma} é Impar Voce Ganhou !\n')
             cont += 1
-            # soma = 0
+            soma = 0
     
         else:
             print(f'Você é impar escolheu o Numero {number} e seu oponente {Ai} logo {soma} é Par Voce perdeu !\n')
