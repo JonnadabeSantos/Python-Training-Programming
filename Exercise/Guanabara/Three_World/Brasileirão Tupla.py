7281c969509c7b46a60eb28def6a9a8b9ecc0b2b
JonnadabeSantos <dabedeveloper@gmail.com>
2023-05-23 07:24:01 -0300

    add_file

---
+++ Exercise/Guanabara/Three_World/Brasileirão Tupla.py
@@ -0,0 +1,6 @@
+brasileirao = ('Botafogo', 'Palmeiras', 'Fluminense', 'Atlético-MG', 'Cruzeiro', 'Flamengo' , 'Athletico-PR', 'São Paulo ', 'Santos', 'Grêmio', 'Fortaleza', 'Bragantino', 'Bahia', 'Cuiabá', 'Internacional', 'Goiás', 'Vasco', 'Corinthians', 'América - MG', 'Coritiba')
+
+print(f'{brasileirao[:5]}\n')
+print(f'{brasileirao[-4:]}\n')
+print(f'{sorted(brasileirao)}\n')
+print(f'{brasileirao.index("Santos")}\n')
---
+++ Exercise/Guanabara/Three_World/random Tupla.py
@@ -0,0 +1,7 @@
+from random import randint
+ai = randint(1,5)
+print(ai)
+
+for tuple in range(ai):
+    machine = (tuple)
+    print(machine)
