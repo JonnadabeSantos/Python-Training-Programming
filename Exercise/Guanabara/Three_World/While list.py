19bee18f22b4f44984cc67f735165796fd269bfb
JonnadabeSantos <dabedeveloper@gmail.com>
2023-05-25 07:40:34 -0300

    Create_While_list

---
+++ Exercise/Guanabara/Three_World/While list.py
@@ -0,0 +1,22 @@
+number = []
+lop = 's'
+while True:
+    
+    if lop in 'Ss':    
+        user = int(input('Type Number list: '))
+
+        if user in number:
+            print('Duplicate number! enter another number...\n')                    
+        else:
+            print('Number successfully added...\n')
+            number.append(user)
+        
+        while True:
+            lop = input('Do you wish to continue? [S/N]: ').strip() [0]
+            if lop in 'SsNn':
+                break
+    
+    elif lop in 'Nn':
+        break            
+           
+print(f'Your list is {number}')
