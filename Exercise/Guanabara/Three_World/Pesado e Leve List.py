f1c3987d746c65463db0278ecde2c41adf08147b
JonnadabeSantos <129608615+JonnadabeSantos@users.noreply.github.com>
2023-06-30 23:50:06 -0300

    pesado e leve final

--- Exercise/Guanabara/Three_World/Pesado e Leve List.py
+++ Exercise/Guanabara/Three_World/Pesado e Leve List.py
@@ -2,10 +2,19 @@ glob = []
 people = []
 leve = []
 pesado = []
+maior = menor = 0
 
 while True:
     people.append(input('Type Name: '))
-    people.append(int(input('Type Yeers Old: ')))
+    people.append(int(input('Your Weight : ')))
+    if len(glob) == 0:
+        maior = menor = people[1]
+    else:
+        if people[1] > maior:
+            maior = people[1]
+        if people[1] < menor:
+            menor = people[1]
+
     glob.append(people[:])
     people.clear()
 
@@ -14,15 +23,13 @@ while True:
         break
 
 for lista in glob:
-    for peso in lista:
-                
-        if peso.isdigit():
-            if peso >= 100:
-                pesado.append(peso)
-            
-            elif peso <= 70:
-                leve.append(peso)
+    if lista[1] >= 100:
+        pesado.append(lista[0])
+    
+    elif lista[1] <= 70:
+        leve.append(lista[0])
 
-print(f'o maior peso foi {max(pesado)} e as pessoas mais pesadas foram {pesado}')
-print(f'O menor peso foi {min(leve)} e as pessoas mais leves são {leve}')
-print(f'O total de pessoas cadastradas foram {len(glob)}')
+
+print(f'o maior peso foi {maior} e as pessoas mais pesadas foram {pesado}')
+print(f'O menor peso foi {menor} e as pessoas mais leves são {leve}')
+print(f'\nO total de pessoas cadastradas foram {len(glob)}')
