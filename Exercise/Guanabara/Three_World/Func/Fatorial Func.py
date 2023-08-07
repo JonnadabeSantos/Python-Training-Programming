056dfe00cea31689a9dddcdcea5925b2c6c30d62
JonnadabeSantos <129608615+JonnadabeSantos@users.noreply.github.com>
2023-08-06 21:14:10 -0300

    Factorial FUnc

---
+++ Exercise/Guanabara/Three_World/Func/Fatorial Func.py
@@ -0,0 +1,25 @@
+def factor(number, show=False):
+    for n in range ( number, 1 , -1 ):
+        if n == number:      
+            s = number * ( n - 1 )
+            if show == True:
+                print(f'{n}', end=' x ')
+
+        else:
+            s = s * ( n -1 )
+            if show == True:
+                print(f'{n}', end=' x ')
+                if n == 2:
+                    print('1', end=' ')
+    print(f'{s}')
+
+
+print( '-' * 40 )
+x = int(input('number: '))
+w = input('want to see the calculation [Y/N}?: ')
+if w in 'Yy':
+    w = True
+else:
+    w = False
+
+factor( x,w )
