393f7ad3c4675da7855f33f44c2eda408b5dc9f1
JonnadabeSantos <129608615+JonnadabeSantos@users.noreply.github.com>
2023-08-11 17:23:13 -0300

    Notas BO

--- Exercise/Guanabara/Three_World/Func/Notas Func.py
+++ Exercise/Guanabara/Three_World/Func/Notas Func.py
@@ -2,13 +2,25 @@ def notas(valor):
     while True:
         turma = dict()
         aluno = list()
+        new = ' '
+        if perg in 'Ss':
+            new = 's'
 
-        while True:            
-            aluno.append(valor)
-            new = input('Deseja informar outra nota [S/N] ?: ')
+        while True:
+            if new in 'Ss':
+                othe = float(input('Infome sua nota: '))            
+                aluno.append(othe)
+            else:
+                aluno.append(valor)
+            
+            while True:
+                new = input('Deseja informar outra nota [S/N] ?: ')
+                if new in 'SsNn':
+                    break
+            
             if new in 'Nn':
                 break
-        
+            
         turma['total'] = len(aluno)
         turma['maior'] = max(aluno)
         turma['menor'] = min(aluno)
@@ -17,9 +29,10 @@ def notas(valor):
         print( '-' * 40 )
         print(turma)
         perg = input('Deseja cadastrar um novo [S/N] ?: ')
+        
         if perg in 'Nn':
             break
-
+        
 
 infor = float(input('Infome sua nota: '))
 notas(infor)
