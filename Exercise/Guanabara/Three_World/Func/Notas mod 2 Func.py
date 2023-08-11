06f623fffdd77d8e9b92c845bebadf74c0112ca9
JonnadabeSantos <129608615+JonnadabeSantos@users.noreply.github.com>
2023-08-11 20:49:16 -0300

    Nota mod 2

---
+++ Exercise/Guanabara/Three_World/Func/Notas mod 2 Func.py
@@ -0,0 +1,49 @@
+def notas( * valor, sit=False ):          
+      large = small = cont = total = 0
+      for note in valor:
+            for ind, tur in enumerate(note):
+                  if ind == 0:
+                        large = small = tur                       
+                  else:                  
+                        if tur > large:
+                              large = tur                   
+                        elif tur < small:
+                              small = tur                                               
+                  total += tur
+                  cont += 1
+
+      turma['Total'] = cont
+      turma['Maior'] = large
+      turma['Menor'] = small
+      turma['Média'] = (total / cont)
+
+      show = input('Do you want to show the situation [ Y/N ]?: ')
+      if show in 'Yy':
+            sit = True
+            if sit:
+                  if turma['Média'] >= 7:
+                        turma['situação'] = 'Excelente!'
+
+                  elif turma['Média'] >= 6:
+                        turma['situação'] = 'Razoável!'
+
+                  elif turma['Média'] >= 5:
+                        turma['situação'] =  'Ruim!' 
+                  else:
+                        turma['situação'] = 'Péssimo!'
+
+                  
+      return turma
+
+            
+turma = dict()
+aluno = list() 
+
+while True:
+      now = float(input('Type Note: '))
+      aluno.append(now)   
+      perg = input('Do you wnat a new note [ Y/N ] ?: ')
+      if perg in 'Nn':
+            break
+
+print(notas(aluno))
