914eceff5de5f7717ac1fcfaed3fa9459d735fa2
JonnadabeSantos <129608615+JonnadabeSantos@users.noreply.github.com>
2023-08-12 18:47:20 -0300

    Mod start

---
+++ Exercise/Guanabara/Three_World/Modularização/Finac/Matem Mod.py
@@ -0,0 +1,5 @@
+from Finac import moeda
+
+p = float(input('Digite o Preço: R$ '))
+print(f'A metade de {p} é {moeda.dobro(p)}')
+
---
+++ Exercise/Guanabara/Three_World/Modularização/Finac/moeda.py
@@ -0,0 +1,11 @@
+def metade(met):
+    return met / 2
+
+def dobro(dob):
+    return dob * 2
+
+def reduzir(val, porc):
+    return val * ( 1 - ( porc / 100 ) )
+
+def aumentar(aum, poc):
+    return aum * ( 1 + ( poc / 100 ) )
