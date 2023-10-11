e6826fb9d39df461cdb1f1234d4127001015397c
JonnadabeSantos <129608615+JonnadabeSantos@users.noreply.github.com>
2023-10-11 12:19:32 -0300

    dict POO

--- POO/Trainer_POO.py
+++ POO/Trainer_POO.py
@@ -7,52 +7,63 @@ def welcome(msg):
 
 
 welcome('Welcome a trainer POO')
+people_list = []
 
 while True:
+    dic = {}
 
-    name = input('what your name ?: ')
-    year_old = int(input('What your years old?: '))
-    height = float(input('what your tall ?: '))
-    work = input('What is your work ?: ')
-    sex = input('Do you Man or Woman ?: ')
+    dic['name'] = input('what your name ?: ')
+    dic['year_old'] = int(input('What your years old?: '))
+    dic['height'] = float(input('what your tall ?: '))
+    dic['work'] = input('What is your work ?: ')
+    dic['sex'] = input('Do you Man or Woman ?: ')
 
-    p1 = People(name, year_old, height, work, sex)
+    people_list.append(dic.copy())
+    print(people_list[0]['name'])
 
-    welcome('Main Menu')
-
-    MenuLista = '''
-    1 - Speak
-    2 - Eat
-    3 - Walk
-    4 - Stop walk
-    5 - Stop eat
-    6 - Stop speak 
-    '''
-    while True:
-        select = int(input(f'{MenuLista}Select opction: '))
-
-        print()
-        if select == 1:
-            p1.Speak(input('Talk about the topic: '))
-
-        elif select == 2:
-            p1.Eat(input('What do you want to eat: '))
-
-        elif select == 3:
-            p1.Walk()
-
-        elif select == 4:
-            p1.StopWalk()
-
-        elif select == 5:
-            p1.StopEat()
-
-        elif select == 6:
-            p1.StopSpeak()
-
-        else:
-            break
-    
-    new_people = input('Do you want to register again [Y/N] ?: ')
+    new_people = input('\nDo you want register again ?: ')
     if new_people in 'Nn':
         break
+
+    for x in range(0, len(new_people)):
+        pass
+    # p1 = People(name, year_old, height, work, sex)
+
+    # welcome('Main Menu')
+
+    # MenuLista = '''
+    # 1 - Speak
+    # 2 - Eat
+    # 3 - Walk
+    # 4 - Stop walk
+    # 5 - Stop eat
+    # 6 - Stop speak 
+    # '''
+    # while True:
+    #     select = int(input(f'{MenuLista}Select opction: '))
+
+    #     print()
+    #     if select == 1:
+    #         p1.Speak(input('Talk about the topic: '))
+
+    #     elif select == 2:
+    #         p1.Eat(input('What do you want to eat: '))
+
+    #     elif select == 3:
+    #         p1.Walk()
+
+    #     elif select == 4:
+    #         p1.StopWalk()
+
+    #     elif select == 5:
+    #         p1.StopEat()
+
+    #     elif select == 6:
+    #         p1.StopSpeak()
+
+    #     else:
+    #         break
+    
+    # new_people = input('Do you want to register again [Y/N] ?: ')
+    # if new_people in 'Nn':
+    #     break
