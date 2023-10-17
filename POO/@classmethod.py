6b9f083119ad7332edc5f7d30afc0dcecca578fa
JonnadabeSantos <129608615+JonnadabeSantos@users.noreply.github.com>
2023-10-16 22:56:53 -0300

    undertand

--- POO/@classmethod.py
+++ POO/@classmethod.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 class Pessoa:
-    ano_atual = 2019
+    ano_atual = date.today().year
+    today = date.today().day
 
     def __init__(self, nome, idade):
         self.nome = nome
@@ -7,12 +10,19 @@ class Pessoa:
 
 
     def get_ano_nascimento(self):
+        print(f'Today is {self.today}')
         print(self.ano_atual - self.idade)
+        
 
     @classmethod
     def por_ano_nascimento(cls, nome, ano_nascimento):
         idade = cls.ano_atual - ano_nascimento
         return cls(nome, idade)
+
+p2 = Pessoa('jon', 32)
+print(p2.idade)
+p2.get_ano_nascimento()
+print()
 
 p1 = Pessoa.por_ano_nascimento('Luiz', 1985)
 print(p1.idade)
