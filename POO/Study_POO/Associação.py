68bc50ddf7df02c703c2acc843bb4a21773a51b8
JonnadabeSantos <129608615+JonnadabeSantos@users.noreply.github.com>
2024-01-03 21:21:44 -0300

    Undestand_associacao

--- POO/Study_POO/Associação.py
+++ POO/Study_POO/Associação.py
@@ -26,3 +26,22 @@ class Caneta:
 
     def escrever( self ):
         print( 'Caneta está escrevendo...' )
+
+class MaquinaDeEscrever:
+     def escrever( self ):
+         print( 'Máquina está escrevendo' )
+
+
+
+escritor = Escritor( 'Joãozinho' )
+caneta = Caneta( 'Bic' )
+maquina = MaquinaDeEscrever()
+
+
+
+escritor.ferramenta = caneta # Assicia com tudo quem tem na Classe Caneta
+escritor.ferramenta.escrever()
+
+escritor.ferramenta = maquina # Assicia com tudo quem tem na Classe Caneta
+escritor.ferramenta.escrever()
+
